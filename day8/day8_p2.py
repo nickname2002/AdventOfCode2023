be8606facc8ddd94495d9d900a00f4ef39259a04
nickname2002 <nickjordan2002@gmail.com>
2023-12-08 11:00:01 +0100

    Started implementing day 8 p2.

---
+++ day8/day8_p2.py
@@ -0,0 +1,97 @@
+
+# Traversing operations
+operations = []
+
+# Dict: (source, destination)
+locations = {}
+current_locations = []
+
+
+# Read locations from input file.
+def read_locations() -> None:
+    global operations, locations
+    input_file = open("day8/input.txt", "r")
+
+    for i, line in enumerate(input_file):
+
+        # Read operations
+        if i == 0:
+            operations = [*line.strip()]
+
+        # Read locations
+        if i >= 2:
+            fragments = line.split(" = ")
+            tuple_fragment = fragments[1].split(", ")
+            
+            # Create destination locations
+            first_loc = tuple_fragment[0].removeprefix("(")
+            second_loc = tuple_fragment[1].removesuffix(")").replace(")", "").strip()
+            
+            # Add location
+            locations[fragments[0]] = [
+                first_loc,
+                second_loc
+            ]
+
+        
+
+# Fill starting locations list
+def get_starting_locations() -> None:
+    global locations, current_locations
+
+    for location in locations.keys():
+        if location[-1] == "A":
+            current_locations.append(location)
+
+
+# Get whether all locations end with 'Z'
+def all_locations_end_with_z() -> bool:
+    global current_locations
+
+    for location in current_locations:
+        if location[-1] != "Z":
+            return False
+
+    return True
+
+
+# Traverse all locations in list with a specific operation
+def traverse_all_locations(operation: str) -> None:
+    global current_locations, locations
+
+    for i, loc in enumerate(current_locations):
+        if operation == "L":
+            current_locations[i] = locations[loc][0]
+        else:
+            current_locations[i] = locations[loc][1]
+
+
+# Get number of steps to reach "ZZZ" from the starting location.
+def get_number_of_steps() -> int:
+    global locations, operations, current_locations
+
+    steps = 0
+
+    while (not all_locations_end_with_z()):
+        for operation in operations:
+
+            # Case location found
+            if all_locations_end_with_z():
+                break 
+
+            # Continue traversion
+            if operation == "L":
+                traverse_all_locations("L")
+            else:
+                traverse_all_locations("R")
+
+            # Update amount of steps taken
+            steps += 1
+
+    return steps
+    
+
+# Program entry point
+read_locations()
+get_starting_locations()
+print(get_number_of_steps())
